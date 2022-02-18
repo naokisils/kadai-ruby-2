f4d2a39bd5f8ca35c67f49f8daf9bd171debf3ba
naoki.sils <silsvlog@gmail.com>
2022-02-18 12:38:33 +0000

    resubmit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,15 @@
 num_max = 100
 def fizzbuzz(num)
    if num % 15==0
-    puts "FizzBuzz"
+     "FizzBuzz"
    elsif num % 3==0
-    puts "Fizz"
+     "Fizz"
    elsif num % 5==0
-    puts "Buzz"
+     "Buzz"
    else
-    p num
+     num
    end
 end  
   (1..num_max).each do |num|
-   fizzbuzz(num)
+   puts fizzbuzz(num)
 end
